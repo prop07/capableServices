a669d0b0f815883b2313c77ab7f80c31b5452807
prop07 <rijalmanoj07@gmail.com>
2024-05-22 08:37:32 +0545

    bug fix

--- app/components/DrawerNav.tsx
+++ app/components/DrawerNav.tsx
@@ -8,8 +8,7 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import { IoMdMenu } from "react-icons/io";
-// import { useSwipeable } from 'react-swipeable';
-
+import { useSwipeable } from 'react-swipeable';
 
 export default function DrawerNav() {
     const [state, setState] = React.useState({
@@ -19,14 +18,6 @@ export default function DrawerNav() {
         right: false,
     });
 
-    // const handlers = useSwipeable({
-    //     onSwipedRight: (eventData) => {
-    //         console.log("Swiped right!", eventData);
-    //         // Add your logic here for what should happen on swipe right
-    //     },
-    // });
-
-
     const toggleDrawer = (anchor, open) => (event) => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
             return;
@@ -34,21 +25,30 @@ export default function DrawerNav() {
         setState({ ...state, [anchor]: open });
     };
 
+    const swipeHandlers = (anchor) => useSwipeable({
+        onSwipedRight: (eventData) => {
+            // console.log("Swiped right!", eventData);
+            // Add your logic here for what should happen on swipe right
+            setState({ ...state, [anchor]: false });
+        },
+    });
+
     const list = (anchor) => (
         <Box
-            // {...handlers}
-            className="px-3 flex"
+            {...swipeHandlers(anchor)}
             sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
             role="presentation"
+            className="px-3 flex"
             onClick={toggleDrawer(anchor, false)}
             onKeyDown={toggleDrawer(anchor, false)}
         >
-            <div className='bg-gray-400 h-20 w-1 rounded-full justify-center mt-[50vh]' ></div>
+            <div className='bg-gray-400 h-20 w-1 rounded-full justify-center mt-[50vh]'></div>
             <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text) => (
                     <ListItem key={text} disablePadding>
                         <ListItemButton>
                             <ListItemIcon>
+                                {/* Replace 'icone' with an actual icon */}
                                 icone
                             </ListItemIcon>
                             <ListItemText primary={text} />
@@ -63,7 +63,9 @@ export default function DrawerNav() {
         <div>
             {['right'].map((anchor) => (
                 <React.Fragment key={anchor}>
-                    <Button className='rounded-md' onClick={toggleDrawer(anchor, true)}> <IoMdMenu size={30} /></Button>
+                    <Button className='rounded-md' onClick={toggleDrawer(anchor, true)}>
+                        <IoMdMenu size={30} />
+                    </Button>
                     <Drawer
                         anchor={anchor}
                         open={state[anchor]}
