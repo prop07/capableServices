271a56f7e1fdb86b932634b5b2cf38310e6e7269
prop07 <rijalmanoj07@gmail.com>
2024-05-28 08:43:23 +0545

    bug fix

--- app/components/UI/Input/DateField.tsx
+++ app/components/UI/Input/DateField.tsx
@@ -1,3 +1,35 @@
+// import * as React from 'react';
+// import TextField from '@mui/material/TextField';
+// import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+// export default function DateField({ name, placeholder, onChange, errors, ...props }) {
+//     const [value, setValue] = React.useState<Date | null>(null);
+//     const today = new Date();
+
+//     return (
+//         <LocalizationProvider dateAdapter={AdapterDateFns}>
+//             <DatePicker
+//                 className='w-full '
+//                 label={placeholder}
+//                 value={value}
+//                 onChange={(newValue) => {
+//                     onChange(newValue); // Call the onChange function to update the form value
+//                 }}
+//                 minDate={today}
+//                 slotProps={{
+//                     textField: {
+//                         variant: 'outlined',S
+//                         error: errors[name],
+//                         helperText: errors[name] ? errors[name].message : null,
+//                     },
+//                 }}
+//             />
+//         </LocalizationProvider>
+//     );
+// }
+
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
@@ -9,14 +41,18 @@ export default function DateField({ name, placeholder, onChange, errors, ...prop
     const today = new Date();
 
     return (
+        <>
         <LocalizationProvider dateAdapter={AdapterDateFns}>
             <DatePicker
-                className='w-full '
+                className="w-full"
                 label={placeholder}
                 value={value}
                 onChange={(newValue) => {
-                    onChange(newValue); // Call the onChange function to update the form value
+                    setValue(newValue);
+                    onChange(newValue)
+                    
                 }}
+                renderInput={(params) => <TextField {...params} />}
                 minDate={today}
                 slotProps={{
                     textField: {
@@ -25,7 +61,12 @@ export default function DateField({ name, placeholder, onChange, errors, ...prop
                         helperText: errors[name] ? errors[name].message : null,
                     },
                 }}
-            />
+                />
         </LocalizationProvider>
+        {/* <p>{errors[name] ? errors[name].message : null}</p> */}
+                </>
     );
 }
+
+
+// "@mui/x-date-pickers": "5.0.3",
