4b55990f376a4e1d1e2038761361da8a72c8d47f
prop07 <rijalmanoj404@gmail.com>
2024-07-05 13:26:44 +0545

    bug fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 import banner from "../../image/contact.jpg"
+import { Metadata } from "next";
+
+export const metadata: Metadata={
+    title:"Contact",
+    description:"Contact Capable services"
+}
 
 
 const Contact = () => {
@@ -17,7 +23,7 @@ const Contact = () => {
                     {/* Gradient overlay for fading effect at the bottom 10% */}
                     <div className="absolute bottom-0 left-0 right-0 h-[0.5%] bg-gradient-to-b from-transparent to-white"></div>
                 </div>
-                <div className="absolute inset-0 bg-background/50 dark:bg-background/70 flex items-center justify-center p-2  mt-20 sm:mt-0 text-center backdrop-blur-sm">
+                <div className="absolute inset-0 bg-background/50 dark:bg-background/70 flex items-center justify-center p-2  mt-20 sm:mt-0 text-center ">
                     <div className="space-y-4 max-w-3xl">
                         <h1 className="text-xl md:text-4xl font-bold tracking-tight text-white">
                             Contact Us
